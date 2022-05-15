e53374407dab4e62bdc846264ac6c7e6eda6213a
vladajanca <vladajanca@gmail.com>
2022-05-15 11:03:22 -0400

    The basic structure of a car is ready, working on advanced feature

---
+++ src/Class02/Main.java
@@ -0,0 +1,8 @@
+package Class02;
+
+public class Main {
+    public static void main(String[] args) {
+        basicFeature car =new basicFeature();
+        
+    }
+}
---
+++ src/Class02/basicFeature.java
@@ -0,0 +1,10 @@
+package Class02;
+
+public class basicFeature {
+        String engineCapacity;
+        String color;
+        String doors;
+        String headLights;
+        String regNo;
+
+}
