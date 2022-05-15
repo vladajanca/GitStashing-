9480b30d452c3307a6bb0d2c096569e6a55e5c0b
vladajanca <vladajanca@gmail.com>
2022-05-15 12:05:27 -0400

    The red Audi is ready with some basic feature

--- src/Class02/Main.java
+++ src/Class02/Main.java
@@ -2,7 +2,12 @@ package Class02;
 
 public class Main {
     public static void main(String[] args) {
-        basicFeature car =new basicFeature();
-        
+        basicFeature Audi =new basicFeature();
+        Audi.color="red";
+        Audi.doors="4";
+        Audi.engineCapacity="1.6";
+        Audi.headLights="led";
+        Audi.regNo="123";
+
     }
 }
